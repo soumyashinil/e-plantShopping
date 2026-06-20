014a15bd3f56c859ac46162b08aa7c4f37fb4844
soumyashinil <soumyaravindren@gmail.com>
2026-06-21 01:47:05 +0530

    updated styles and bg image added functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
     items: [], // Initialize items as an empty array
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const { name, image, cost } = action.payload;
+
+      const existingItem = state.items.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter((item) => item.name !== action.payload);
+      console.log("Payload:", action.payload);
     },
-    updateQuantity: (state, action) => {
 
-    
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const updateQuantity = state.items.find((item) => item.name === name);
+      if (updateQuantity) {
+        updateQuantity.quantity = quantity;
+      }
     },
   },
 });
